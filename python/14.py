6b8881ac5f8491d37b363688b1b906720ac49045
Mikkel <mikkj17@student.sdu.dk>
2021-01-26 15:06:44 +0100

    Solved part 2 of day 14

--- python/14.py
+++ python/14.py
@@ -10,6 +10,12 @@ mem[7] = 101
 mem[8] = 0\
 """
 
+test2 = """\
+mask = 000000000000000000000000000000X1001X
+mem[42] = 100
+mask = 00000000000000000000000000000000X0XX
+mem[26] = 1\
+"""
 
 def parse(inp: str) -> List[Tuple[str, str]]:
     ret = list()
@@ -18,11 +24,11 @@ def parse(inp: str) -> List[Tuple[str, str]]:
         ret.append((assign, value))
     return ret
 
-def mask_bits(mask: str, bits: str) -> str:
+def mask_value(mask: str, value: str) -> str:
     ret = ''
     for index, bit in enumerate(mask):
         if bit == 'X':
-            ret += bits[index]
+            ret += value[index]
         else:
             ret += bit
     return ret
@@ -34,19 +40,52 @@ def part1(program: List[Tuple[str, str]]) -> int:
             mask = value
         else:
             address = re.match('mem\[(\d+)\]', assignment)[1]
-            memory[address] = int(mask_bits(mask, bin(int(value))[2:].zfill(36)), 2)
+            memory[address] = int(mask_value(mask, bin(int(value))[2:].zfill(36)), 2)
 
     return sum(value for _, value in memory.items())
 
-def part2() -> int:
-    pass
+
+def mask_address(mask: str, address: str) -> str:
+    ret = ''
+    for index, bit in enumerate(mask):
+        if bit == '0':
+            ret += address[index]
+        else:
+            ret += bit
+    return ret
+
+def compute_addresses(address: str) -> List[str]:
+    ret = list()
+    count = address.count('X')
+    for i in range(2**count):
+        replacement = bin(i)[2:].zfill(count)
+        binary = address
+        for bit in replacement:
+            binary = binary.replace('X', bit, 1)
+        ret.append(int(binary, 2))
+    return ret
+
+def part2(program: List[Tuple[str, str]]) -> int:
+    memory = dict()
+    for assignment, value in program:
+        if assignment == 'mask':
+            mask = value
+        else:
+            decimal_address = re.match('mem\[(\d+)\]', assignment)[1]
+            address = mask_address(mask, bin(int(decimal_address))[2:].zfill(36))
+            addresses = compute_addresses(address)
+            for address in addresses:
+                memory[address] = int(value)
+
+    return sum(value for _, value in memory.items())
+
 
 def main():
     filepath = "../inputs/14.txt"
     with open(filepath) as f:
         content = f.read()
     parsed = parse(content)
-    print(part1(parsed))
+    print(part2(parsed))
 
 if __name__ == "__main__":
     main()
