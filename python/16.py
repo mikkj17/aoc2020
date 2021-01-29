244ee7d395cbaab6eb2da10cf30d9b418d3652e4
Mikkel <mikkj17@student.sdu.dk>
2021-01-29 12:37:52 +0100

    Finished day 16 with some ugly code

--- python/16.py
+++ python/16.py
@@ -1,7 +1,10 @@
 import pprint
 import re
+import numpy as np
 
 from typing import List, Tuple, Dict
+from collections import defaultdict
+from functools import reduce
 
 test = """\
 class: 1-3 or 5-7
@@ -37,7 +40,7 @@ class Rule:
     def __init__(self, field: str, ranges: Tuple[range, range]):
         self.field = field
         self.ranges = ranges
-    
+
     def __repr__(self):
         return f'field: {self.field}; ranges: {self.ranges}'
 
@@ -84,29 +87,58 @@ def part1(document: Tuple[List[Rule], List[int], List[List[int]]]) -> int:
     rules, _, nearby = document
     return helper(rules, nearby)[0]
 
-def determine_order(rules: List[Rule], tickets: List[List[int]]) -> Dict[Rule, List[int]]:
-    order = dict()  # maybe use defaultdict
-    
-    for index, rule in enumerate(rules):
-        # TODO: implement logic
-        pass
+def determine_order(rules: List[Rule], tickets: List[List[int]]) -> List[Tuple[int, int]]:   # holy fuck this is ugly
+    possibilities = list()
 
+    for rule in rules:
+        possible = list()
+        for index in range(len(rules)):
+            valid = True
+            for ticket in tickets:
+                value = ticket[index]
+                if value not in rule.ranges[0] and value not in rule.ranges[1]:
+                    valid = False
+                    break
+            if valid:
+                possible.append(index)
+        possibilities.append(possible)
 
+    order = list()
+    while len(order) < len(rules):
+        for index, possible in enumerate(possibilities):
+            if len(possible) == 1:
+                field_index = possible[0]
+                order.append((index, field_index))
+                break
+        for possible in possibilities:
+            try:
+                possible.remove(field_index)
+            except:
+                pass
     return order
 
 
-def part2(document: Tuple[List[Rule], List[int], List[List[int]]]) -> int:
+def part2(document: Tuple[List[Rule], List[int], List[List[int]]]) -> int:  # also super ugly
     rules, ticket, nearby = document
     valid_tickets = helper(rules, nearby)[1]
-    solutions = determine_order(rules, valid_tickets)
-    pprint.pprint(solutions)
+    order = determine_order(rules, valid_tickets)
+    departure_indices = [index for index, rule in enumerate(rules) if rule.field.startswith('departure')]
+    corresponding_indices = list()
+    for index in departure_indices:
+        for idx, field_idx in order:
+            if index == idx:
+                corresponding_indices.append(field_idx)
+                break
+
+    return reduce(lambda x, y: x*y, [value for index, value in enumerate(ticket) if index in corresponding_indices])
+
 
 
 def main():
     filepath = "../inputs/16.txt"
     with open(filepath) as f:
         content = f.read()
-    parsed = parse(notes)
+    parsed = parse(content)
     print(part2(parsed))
 
 if __name__ == "__main__":
